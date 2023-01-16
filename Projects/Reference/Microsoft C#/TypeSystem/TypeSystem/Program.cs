531a2189b1bfca0c0f7da7b8a2f57517b40aa093
Jaloliddin <jalol101.ghost@yandex.com>
2023-01-16 23:32:45 +0500

    Add project files.

---
+++ Projects/Reference/Microsoft C#/TypeSystem/TypeSystem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TypeSystemInCsharp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("===============================");
+            Console.WriteLine("Microsoft C# reference");
+            Console.WriteLine("Section: Type system");
+            Console.WriteLine("===============================\n\n\n");
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# this folder contains everything about c# programming language
+# it is complete reference of the c# programming language
+# it can also contain some projects like console application that can access databases
+# also the projects like web applications using asp.net core and blazor web assembly
+# good luck Jaloliddin
+# edited in:  16 Jan 2023 22:55 or 10:55PM
+
+
+
+
+# ver-1
+# description
+# ...
