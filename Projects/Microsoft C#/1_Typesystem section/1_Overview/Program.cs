ae585354c3372b1ccff23d85639f63a57336c7b6
Jaloliddin <jalol101.ghost@yandex.com>
2023-01-21 20:47:36 +0500

    commit

---
+++ Projects/Microsoft C#/1_Typesystem section/1_Overview/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Overview
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...\n");
+            Console.WriteLine("""
+                =================================
+                      Microsoft C# reference     
+                =================================
+
+
+                """);
+
+            string triangle = """
+                |\
+                | \
+                |  \
+                |___\
+                """;
+            Console.WriteLine(triangle + "\nThis is triangle :)");
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
+
+namespace SourceCode
+{
+    public class Overview
+    {
+
+    }
+}
